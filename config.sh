10e56bc45a9b6b1f56fcd5018225965c7bbcd822
František Dvořák <valtri@civ.zcu.cz>
2015-11-10 13:00:51 +0100

    Create also repo files.

--- config.sh
+++ config.sh
@@ -1,4 +1,6 @@
 #COPR_HOST="myriad6.zcu.cz"
+#GPG_KEY_URL='http://scientific.zcu.cz/repos/jenkins-builder.asc'
+#GPG_SHORT_KEY_URL='http://scientific.zcu.cz/repos/jenkins-builder-sl5.asc'
 #PRINCIPAL="jenkins/emian.zcu.cz@ZCU.CZ"
 #KEYTAB="${HOME}/krb5kt_jenkins"
 #JENKINS_JAR=/var/lib/tomcat6/webapps/jenkins/WEB-INF/slave.jar
