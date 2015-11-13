5f3d9fddabd63fbd45abd5d065bd76c1cd41d668
František Dvořák <valtri@civ.zcu.cz>
2015-11-13 15:29:18 +0100

    Partial support for RHEL 5 and remove hacks.

--- examples/config.sh
+++ examples/config.sh
@@ -1,6 +1,5 @@
 #COPR_HOST="myriad6.zcu.cz"
 #GPG_KEY_URL='http://scientific.zcu.cz/repos/jenkins-builder.asc'
-#GPG_SHORT_KEY_URL='http://scientific.zcu.cz/repos/jenkins-builder-sl5.asc'
 #PRINCIPAL="jenkins/emian.zcu.cz@ZCU.CZ"
 #KEYTAB="${HOME}/krb5kt_jenkins"
 #JENKINS_JAR=/var/lib/tomcat6/webapps/jenkins/WEB-INF/slave.jar
